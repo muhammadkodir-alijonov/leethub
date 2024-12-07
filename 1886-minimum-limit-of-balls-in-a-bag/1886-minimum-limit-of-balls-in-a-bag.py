1f1efd19a3c14ecda4cb99cc3b819eedd2efeb8d
mukhammadkodir <98277057+mukhammadkodir-alijonov@users.noreply.github.com>
2024-12-07 21:16:55 +0500

    Time: 563 ms (79.47%), Space: 29.1 MB (34.09%) - LeetHub

--- 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.py
+++ 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.py
@@ -3,20 +3,14 @@ from typing import List
 
 class Solution:
     def minimumSize(self, nums: List[int], maxOperations: int) -> int:
-        left, right = 1, max(nums)
+        def check(x):
+            return sum((num - 1) // x for num in nums) <= maxOperations
 
+        left, right = 1, max(nums)
         while left < right:
             mid = (left + right) // 2
-            if self.count_operations(nums, mid) <= maxOperations:
+            if check(mid):
                 right = mid
             else:
                 left = mid + 1
-
         return left
-
-    def count_operations(self, nums: List[int], max_size: int) -> int:
-        operations = 0
-        for num in nums:
-            if num > max_size:
-                operations += (num - 1) // max_size
-        return operations
