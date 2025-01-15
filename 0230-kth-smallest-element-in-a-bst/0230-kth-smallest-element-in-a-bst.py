d3b5baaf4be0633c4049c28444b3ed4014d4e685
muhammadkodir <98277057+muhammadkodir-alijonov@users.noreply.github.com>
2025-01-15 10:12:11 +0500

    Time: 0 ms (100%), Space: 21.1 MB (36.24%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        stack = []
+        current = root
+        count = 0
+
+        while stack or current:
+            while current:
+                stack.append(current)
+                current = current.left
+
+            current = stack.pop()
+            count += 1
+
+            if count == k:
+                return current.val
+
+            current = current.right
+
+        return None 
