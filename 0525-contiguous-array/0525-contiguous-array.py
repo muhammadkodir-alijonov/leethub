d255db8b79b565e14c94380d46c2b97b8ff64218
muhammadkodir <98277057+muhammadkodir-alijonov@users.noreply.github.com>
2025-03-20 09:42:24 +0500

    Time: 109 ms (36.15%), Space: 23.4 MB (86.7%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -1,17 +1,18 @@
+from typing import List
+
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        # 0 and 1 count should be equal
-        # find out longest len of nums
-        count = 0
+        # imagine 0 is -1 and 1 is +1 , count is summ of distacne of 0 and 1
+        count = 0 # if the count occured before, 0 and 1 distamce are equal:
         max_len = 0
-        count_dict = {0: -1}
-        for i in range(len(nums)):
+        dic_ = {0:-1}
+        for i  in range(len(nums)):
             if nums[i] == 0:
                 count -= 1
-            else:
+            if nums[i] == 1:
                 count += 1
-            if count in count_dict:
-                max_len = max(max_len, i - count_dict[count])
+            if count in dic_: # is that if count has already occurred before
+                max_len = max(max_len, i - dic_[count]) # we calculate the distance from the first occurrence of count to the current index i.
             else:
-                count_dict[count] = i
+                dic_[count] = i
         return max_len
