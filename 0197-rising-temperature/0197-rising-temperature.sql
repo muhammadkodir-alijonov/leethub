a97c026b80627a876195002ef8c3d44765ee93a2
muhammadkodir <98277057+muhammadkodir-alijonov@users.noreply.github.com>
2025-03-29 23:27:00 +0500

    Time: 556 ms (90.22%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,7 +1,10 @@
-# Write your MySQL query statement below
-select 
-    p1.id 
-from 
-    weather p1, weather p2 
-where 
-    p2.recorddate = DATE_SUB(p1.recorddate, INTERVAL 1 Day) and p1.temperature > p2.temperature; 
+/* Write your PL/SQL query statement below */
+SELECT 
+    today.id
+FROM 
+    weather today
+LEFT JOIN 
+    weather yesterday
+    ON today.recorddate - 1 = yesterday.recorddate
+WHERE 
+    today.temperature > yesterday.temperature;
