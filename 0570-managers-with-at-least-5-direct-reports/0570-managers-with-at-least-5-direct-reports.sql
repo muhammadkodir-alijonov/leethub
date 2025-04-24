0f685e8f4afc4f796b5f43f1df188a6ed7453444
muhammadkodir <98277057+muhammadkodir-alijonov@users.noreply.github.com>
2025-04-24 16:32:18 +0500

    Time: 418 ms (81.64%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,1 +1,13 @@
 /* Write your PL/SQL query statement below */
+SELECT 
+    e1.name
+FROM 
+    employee e1
+JOIN 
+    employee e2 
+ON 
+    e1.id = e2.managerId
+GROUP BY 
+    e1.id, e1.name 
+HAVING 
+    COUNT(e2.id) >= 5;
