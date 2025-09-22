7e4d074ae942c7b255492d95ebafc02d01ce8dad
muhammadkodir <98277057+muhammadkodir-alijonov@users.noreply.github.com>
2025-09-22 15:35:52 +0500

    Update 0585-investments-in-2016.sql

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,4 +1,8 @@
 /* Write your PL/SQL query statement below */
+-- 1. summ all inestmant in 2016 for all pid         
+-- 2. where: value 2015 one or more
+-- 3. not located same city and other placeholder(let and lon)
+-- 4. res should be round 2 decemal
 select round(sum(q.tiv_2016), 2) as tiv_2016
   from Insurance q
  where q.tiv_2015 in (select t.tiv_2015
