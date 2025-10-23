2a62347e8974256cdaca4a865d2052b4418eb09e
muhammadkodir <98277057+muhammadkodir-alijonov@users.noreply.github.com>
2025-10-23 11:01:27 +0500

    Time: 31 ms (6.42%), Space: 15.3 MB (100%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -1,31 +1,22 @@
-# Definition for singly-linked list.
-
-# Input: head = [1,2,3,4,5]
-# Output: [1,3,5,2,4]
-
-# We must solve the problem in O(1) extra space complexity and O(n) time complexity.
-# separate list first odd and then even indexes' number
-
-from typing import Optional
-
-class ListNode:
+class ListNode(object):
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
-class Solution:
-    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+class Solution(object):
+    def oddEvenList(self, head):
         if not head or not head.next:
             return head
+
         odd = head
         even = head.next
-        even_head = even
+        even_head = even  # Keep track of the start of the even list
+
         while even and even.next:
-            odd.next = odd.next.next
-            print(odd.val)
-            even.next = even.next.next
-            print(even.val)
+            odd.next = even.next
             odd = odd.next
+            even.next = odd.next
             even = even.next
-        odd.next = even_head
+
+        odd.next = even_head  # Append the even list to the end of the odd list
         return head
