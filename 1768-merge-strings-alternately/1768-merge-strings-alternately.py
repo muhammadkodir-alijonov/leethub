3918ef8179b3a1780babc823530d2eeee89a451e
muhammadkodir <98277057+muhammadkodir-alijonov@users.noreply.github.com>
2026-01-20 11:11:31 +0500

    Time: 27 ms (97.63%), Space: 16.5 MB (100%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,21 +1,7 @@
-# req: 1 <= word1.length, word2.length <= 100
-# word1 and word2 consist of lowercase English letters.
-
-
 class Solution:
     def mergeAlternately(self,word1: str, word2: str) -> str:
-        word1_l = len(word1)
-        word2_l = len(word2)
-        n = min(word1_l, word2_l)
-        ans = ''
-        for i in range(n):
-            ans += word1[i]
-            ans += word2[i]
-        ans += word1[n:]
-        ans += word2[n:]
-        return ans
-
-if __name__ == '__main__':
-    s = Solution()
-    res = s.mergeAlternately('abcd','pq')
-    print('res: ' + res)
+        result = ""
+        for i in range(min(len(word1), len(word2))):
+            result += word1[i] + word2[i]
+        result += word1[len(word2):] + word2[len(word1):]
+        return result
