dab1f339a310420b25af9bd057764bdb9d8a9777
muhammadkodir <98277057+muhammadkodir-alijonov@users.noreply.github.com>
2026-07-07 11:50:35 +0500

    Time: 131 ms (5.18%), Space: 35.6 MB (16.25%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
@@ -1,9 +1,18 @@
+# if nums  null return []
+# 1. we can use hashmap to calculate how many twise elemetn in list
+# 2. Sort and calculate
+
 from typing import List
 from collections import Counter
 
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
-        curr = Counter(nums)
+        curr = dict()
+        for i in nums:
+            if i in curr:
+                curr[i] += 1
+            else:
+                curr[i] = 1
         res = []
         for k, v in curr.items():
             print(f"The key is '{k}' and the value is '{v}'")
