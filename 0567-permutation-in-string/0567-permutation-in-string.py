5c248062a964663477b85d70a2601eb90e21ded0
muhammadkodir <98277057+muhammadkodir-alijonov@users.noreply.github.com>
2026-07-24 10:06:12 +0500

    Time: 1020 ms (15.68%), Space: 19.4 MB (52.9%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,10 +1,18 @@
+from collections import Counter
+
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         len_s1 = len(s1)
         len_s2 = len(s2)
-
+        
+        if len_s1 > len_s2:
+            return False
+            
+        s1_count = Counter(s1)
+        
         for i in range(len_s2 - len_s1 + 1):
             window = s2[i : i + len_s1]
-            if sorted(s1) == sorted(window):
+            if Counter(window) == s1_count:
                 return True
+                
         return False
